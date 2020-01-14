d0acf73b0e4ab969a7cb07720a8ac1c07838afbc
Victor S Nakasone <victor.nakasone@fatec.sp.gov.br>
2020-01-13 21:53:40 -0300

    Validação dados categorias de carro

--- spec/features/admin_register_car_category_spec.rb
+++ spec/features/admin_register_car_category_spec.rb
@@ -34,4 +34,21 @@ feature 'Admin register Car Category' do
         expect(page).to have_content('Seguro contra terceiros não pode ficar vazio')
 
     end
+
+    scenario 'and daily rate, car insurance, third party insurance must be greater than 0' do
+        visit root_path
+        
+        click_on 'Categorias de carro'
+        click_on 'Cadastrar categoria de carro'
+
+        fill_in 'Nome', with: 'Sedã'
+        fill_in 'Diária', with: -1
+        fill_in 'Seguro do automóvel', with: -1
+        fill_in 'Seguro contra terceiros', with: -1
+        click_on 'Enviar'
+
+        expect(page).to have_content('Daily rate must be greater than 0')
+        expect(page).to have_content('Car insurance must be greater than 0')
+        expect(page).to have_content('Third party insurance must be greater than 0')
+    end
 end
