0d638e4fc589aea02394a5b798b037a33c5bc266
Victor S Nakasone <victor.nakasone@fatec.sp.gov.br>
2020-01-20 23:26:49 -0300

    Se aplicado, será criado validações customizadas para Locações Antes a data de início e fim podiam ser qualquer dia aleatório

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -2,4 +2,22 @@ class Rental < ApplicationRecord
   belongs_to :client
   belongs_to :car_category
   belongs_to :user
+  
+  validate :start_date_cannot_be_in_the_past
+  validate :start_date_cannot_be_greater_than_end_date
+  validates :start_date, presence: {message: 'não pode ficar vazio'}
+  validates :end_date, presence: {message: 'não pode ficar vazio'}
+
+  def start_date_cannot_be_in_the_past
+    if start_date.present? && start_date < Date.current 
+      errors.add(:start_date, 'não pode estar no passado')
+    end
+  end
+
+  def start_date_cannot_be_greater_than_end_date
+    if start_date.present? && end_date.present? && start_date > end_date 
+      errors.add(:start_date, 'não pode ser maior que a data final')
+    end
+  end
+  
 end
--- spec/models/rental_spec.rb
+++ spec/models/rental_spec.rb
@@ -1,5 +1,46 @@
 require 'rails_helper'
 
 describe Rental do
-  
+  describe '#start_date_cannot_be_in_the_past' do
+    it 'should prevent start date in the past' do
+      rental = Rental.create(start_date: 1.day.ago)
+      rental.valid?
+
+      expect(rental.errors.full_messages).to include('Start date não pode estar no passado')
+    end
+  end
+
+  describe '#start_date_cannot_be_greater_than_end_date' do
+    it 'should prevent start date to be greater than end date' do
+      rental = Rental.create(start_date: 1.day.from_now, end_date: Date.current)
+      rental.valid?
+
+      expect(rental.errors.full_messages).to include('Start date não pode ser maior que a data final')
+    end
+  end
+
+  describe '#fields_cannot_be_blank' do
+    it 'should prevent start date to be blank' do
+      rental = Rental.create(start_date: nil)
+      rental.valid?
+
+      expect(rental.errors.full_messages).to include('Start date não pode ficar vazio')
+    end
+
+    it 'should prevent end date to be blank' do
+      rental = Rental.create(end_date: nil)
+      rental.valid?
+
+      expect(rental.errors.full_messages).to include('End date não pode ficar vazio')
+    end
+
+    it 'should prevent start date and end date to be blank' do
+      rental = Rental.create(start_date: nil, end_date: nil)
+      rental.valid?
+
+      expect(rental.errors.full_messages).to include('Start date não pode ficar vazio')
+      expect(rental.errors.full_messages).to include('End date não pode ficar vazio')
+
+    end
+  end
 end
