746e0a89010fd95531c8705924df7db7017d406c
Victor S Nakasone <victor.nakasone@fatec.sp.gov.br>
2020-01-23 08:34:18 -0300

    Se aplicado, irá permitir criar carros via API Antes, só era possível pela base. Teste de requests criados

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -9,4 +9,16 @@ class Api::V1::CarsController < Api::V1::ApiController
 
     head 404
   end
+  def create
+    @car = Car.new(car_params)
+    return render json: @car, status: :ok if @car.save
+
+    head 412
+  end
+
+  private
+
+  def car_params
+    params.permit(:license_plate, :color, :mileage, :car_model_id)
+  end
 end
--- spec/requests/car_management_spec.rb
+++ spec/requests/car_management_spec.rb
@@ -41,11 +41,12 @@ describe 'Car Management' do
       other_car = Car.create!(car_model: car_model, license_plate: 'DEF5678',
                         color: 'Prata', mileage: 50.99)
       
-      get '/api/v1/cars'
+      get api_v1_cars_path
 
       json = JSON.parse(response.body, symbolize_names: true)
 
       expect(response).to have_http_status(:ok)
+
       expect(json[0][:car_model_id]).to eq car.car_model.id
       expect(json[0][:license_plate]).to eq car.license_plate
       expect(json[0][:color]).to eq car.color
@@ -54,6 +55,27 @@ describe 'Car Management' do
       expect(json[1][:license_plate]).to eq other_car.license_plate
       expect(json[1][:color]).to eq other_car.color
       expect(json[1][:mileage]).to eq '50.99'
+    end
+  end
+  context 'create' do
+    it 'successfully' do
+      manufacturer = Manufacturer.create!(name: 'Fiat')
+      car_category = CarCategory.create!(name: 'A', daily_rate: 36.5,
+                                         car_insurance: 32.90,
+                                         third_party_insurance: 30.90)
+      car_model = CarModel.create!(manufacturer: manufacturer,
+                                   car_category: car_category)
+
+      post '/api/v1/cars', params: {license_plate: 'DBZ9090', color: 'Vermelho',
+                                   mileage: 70.90, car_model_id: car_model.id}
+      
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to have_http_status(:ok)
+      expect(json[:license_plate]).to eq 'DBZ9090'
+      expect(json[:color]).to eq 'Vermelho'
+      expect(json[:mileage]).to eq '70.9'
+      expect(json[:car_model_id]).to eq car_model.id
     end
   end
 end
