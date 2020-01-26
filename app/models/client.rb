2d802566413329b03b90158f7ba4acb15d289c4e
Victor S Nakasone <victor.nakasone@fatec.sp.gov.br>
2020-01-25 22:57:15 -0300

    Se aplicado, validações para Clientes Campos devem estar presentes e limitação de caracteres

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,5 +1,10 @@
 class Client < ApplicationRecord
   has_many :rentals
+
+  validates :name, :email, :cpf, presence: {message: 'não pode ficar vazio'}
+  validates :name, length: {maximum: 64, message: 'muito grande'}
+  validates :email, length: {maximum: 128, message: 'muito grande'}
+  validates :cpf, length: {maximum: 14, message: 'muito grande'}
   def identification
     "#{cpf} - #{name}"
   end
--- spec/features/user_register_client_spec.rb
+++ spec/features/user_register_client_spec.rb
@@ -24,4 +24,49 @@ feature 'User register client' do
 
     expect(current_path).to eq(new_user_session_path)
   end
+
+  scenario 'and fields must be present' do
+    user = User.create!(email: 'teste@teste.com', password: '123456')
+
+    login_as(user, scope: :user)
+    visit new_client_path
+    click_on 'Enviar'
+
+    expect(page).to have_content('Name não pode ficar vazio')
+    expect(page).to have_content('Email não pode ficar vazio')
+    expect(page).to have_content('Cpf não pode ficar vazio')
+  end
+
+  scenario 'and name must be less or equal to 64' do
+    user = User.create!(email: 'teste@teste.com', password: '123456')
+
+    login_as(user, scope: :user)
+    visit new_client_path
+    fill_in 'Nome', with: 'a' * 65
+    click_on 'Enviar'
+
+    expect(page).to have_content('Name muito grande')
+  end
+
+  scenario 'and name must be less or equal to 128' do
+    user = User.create!(email: 'teste@teste.com', password: '123456')
+
+    login_as(user, scope: :user)
+    visit new_client_path
+    fill_in 'Email', with: 'a' * 129
+    click_on 'Enviar'
+
+    expect(page).to have_content('Email muito grande')
+  end
+
+  scenario 'and CPF must be equal to 14' do
+    user = User.create!(email: 'teste@teste.com', password: '123456')
+
+    login_as(user, scope: :user)
+    visit new_client_path
+    fill_in 'CPF', with: 'a' * 15
+    click_on 'Enviar'
+
+    expect(page).to have_content('Cpf muito grande')
+  end
 end
