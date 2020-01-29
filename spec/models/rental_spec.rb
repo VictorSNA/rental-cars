287e18af6f5e5f288b4d72027cfa2a6249b1879a
Victor S Nakasone <victor.nakasone@fatec.sp.gov.br>
2020-01-29 09:25:19 -0300

    Se aplicado, irá aplicar boas práticas ao teste de model de Rental

--- spec/models/rental_spec.rb
+++ spec/models/rental_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 describe Rental do
-
   describe '#start_date_cannot_be_in_the_past' do
     it 'should prevent start date in the past' do
       rental = Rental.create(start_date: 1.day.ago)
       rental.valid?
 
-      expect(rental.errors.full_messages).to include('Start date não pode estar no passado')
+      expect(rental.errors.full_messages).to include('Start date não '\
+                                                     'pode estar no passado')
     end
   end
 
@@ -16,7 +16,9 @@ describe Rental do
       rental = Rental.create(start_date: 1.day.from_now, end_date: Date.current)
       rental.valid?
 
-      expect(rental.errors.full_messages).to include('Start date não pode ser maior que a data final')
+      expect(rental.errors.full_messages).to include('Start date não '\
+                                                     'pode ser maior que '\
+                                                     'a data final')
     end
   end
 
@@ -25,67 +27,86 @@ describe Rental do
       rental = Rental.create(start_date: nil)
       rental.valid?
 
-      expect(rental.errors.full_messages).to include('Start date não pode ficar vazio')
+      expect(rental.errors.full_messages).to include('Start date não '\
+                                                     'pode ficar vazio')
     end
 
     it 'should prevent end date to be blank' do
       rental = Rental.create(end_date: nil)
       rental.valid?
 
-      expect(rental.errors.full_messages).to include('End date não pode ficar vazio')
+      expect(rental.errors.full_messages).to include('End date não '\
+                                                     'pode ficar vazio')
     end
 
     it 'should prevent start date and end date to be blank' do
       rental = Rental.create(start_date: nil, end_date: nil)
       rental.valid?
 
-      expect(rental.errors.full_messages).to include('Start date não pode ficar vazio')
-      expect(rental.errors.full_messages).to include('End date não pode ficar vazio')
+      expect(rental.errors.full_messages).to include('Start date não '\
+                                                     'pode ficar vazio')
+      expect(rental.errors.full_messages).to include('End date não '\
+                                                     'pode ficar vazio')
     end
 
     describe '#car_statuses' do
       it 'should change car status to unavaliable' do
-      user = User.create!(email: 'teste@teste.com', password: '123456')
-      client = Client.create!(name: 'Fulano da Silva', cpf: '127.587.748-60',
-                              email: 'fulanodasilva@teste.com')
-      manufacturer = Manufacturer.create!(name: 'Renault')
-      car_category = CarCategory.create!(name: 'AM', daily_rate: 46.54, car_insurance: 28,
-                                         third_party_insurance: 10)
-      car_model = CarModel.create!(name: 'Kwid', year: '2020', manufacturer: manufacturer,
-                                   motorization: '1.0', car_category: car_category,
-                                   fuel_type: 'Flex')
-      car = Car.create!(license_plate: 'ABC1234', color: 'Branco', car_model: car_model,
-                        mileage: 10000)
-      rental = Rental.create!(code: '1212', start_date: Date.current, end_date: Date.current,
-                              client: client, car_category: car_category, user: user)
-      CarRental.create(car: car, rental: rental, daily_rate: 30, start_mileage: 20,
-                       car_insurance: 30, third_party_insurance: 300)
-      
-      result = rental.car_statuses
+        user = User.create!(email: 'teste@teste.com', password: '123456')
+        client = Client.create!(name: 'Fulano da Silva', cpf: '127.587.748-60',
+                                email: 'fulanodasilva@teste.com')
+        manufacturer = Manufacturer.create!(name: 'Renault')
+        car_category = CarCategory.create!(name: 'AM', daily_rate: 46.54,
+                                           car_insurance: 28,
+                                           third_party_insurance: 10)
+        car_model = CarModel.create!(name: 'Kwid', year: '2020',
+                                     manufacturer: manufacturer,
+                                     motorization: '1.0',
+                                     car_category: car_category,
+                                     fuel_type: 'Flex')
+        car = Car.create!(license_plate: 'ABC1234', color: 'Branco',
+                          car_model: car_model, mileage: 10_000)
+        rental = Rental.create!(code: '1212', start_date: Date.current,
+                                end_date: Date.current,
+                                client: client,
+                                car_category: car_category,
+                                user: user)
+        CarRental.create(car: car, rental: rental, daily_rate: 30,
+                         start_mileage: 20,
+                         car_insurance: 30,
+                         third_party_insurance: 300)
 
-      expect(result).to eq 'unavaliable'
-  
+        result = rental.car_statuses
+
+        expect(result).to eq 'unavaliable'
       end
       it 'should change car status to avaliable' do
         user = User.create!(email: 'teste@teste.com', password: '123456')
         client = Client.create!(name: 'Fulano da Silva', cpf: '127.587.748-60',
                                 email: 'fulanodasilva@teste.com')
         manufacturer = Manufacturer.create!(name: 'Renault')
-        car_category = CarCategory.create!(name: 'AM', daily_rate: 46.54, car_insurance: 28,
+        car_category = CarCategory.create!(name: 'AM', daily_rate: 46.54,
+                                           car_insurance: 28,
                                            third_party_insurance: 10)
-        car_model = CarModel.create!(name: 'Kwid', year: '2020', manufacturer: manufacturer,
-                                     motorization: '1.0', car_category: car_category,
+        car_model = CarModel.create!(name: 'Kwid', year: '2020',
+                                     manufacturer: manufacturer,
+                                     motorization: '1.0',
+                                     car_category: car_category,
                                      fuel_type: 'Flex')
-        car = Car.create!(license_plate: 'ABC1234', color: 'Branco', car_model: car_model,
-                          mileage: 10000)
-        rental = Rental.new(code: '1212', start_date: 2.day.ago, end_date: 1.day.ago,
-                                client: client, car_category: car_category, user: user)
+        car = Car.create!(license_plate: 'ABC1234', color: 'Branco',
+                          car_model: car_model,
+                          mileage: 10_000)
+        rental = Rental.new(code: '1212', start_date: 2.days.ago,
+                            end_date: 1.day.ago,
+                            client: client, car_category: car_category,
+                            user: user)
         rental.save(validate: false)
-        CarRental.create(car: car, rental: rental, daily_rate: 30, start_mileage: 20,
-                         car_insurance: 30, third_party_insurance: 300)
-        
+        CarRental.create(car: car, rental: rental, daily_rate: 30,
+                         start_mileage: 20,
+                         car_insurance: 30,
+                         third_party_insurance: 300)
+
         result = rental.car_statuses
-        
+
         expect(result).to eq 'avaliable'
       end
 
@@ -94,22 +115,34 @@ describe Rental do
         client = Client.create!(name: 'Fulano da Silva', cpf: '127.587.748-60',
                                 email: 'fulanodasilva@teste.com')
         manufacturer = Manufacturer.create!(name: 'Renault')
-        car_category = CarCategory.create!(name: 'AM', daily_rate: 46.54, car_insurance: 28,
+        car_category = CarCategory.create!(name: 'AM', daily_rate: 46.54,
+                                           car_insurance: 28,
                                            third_party_insurance: 10)
-        car_model = CarModel.create!(name: 'Kwid', year: '2020', manufacturer: manufacturer,
-                                     motorization: '1.0', car_category: car_category,
+        car_model = CarModel.create!(name: 'Kwid', year: '2020',
+                                     manufacturer: manufacturer,
+                                     motorization: '1.0',
+                                     car_category: car_category,
                                      fuel_type: 'Flex')
-        car = Car.create!(license_plate: 'ABC1234', color: 'Branco', car_model: car_model,
-                          mileage: 10000)
-        rental = Rental.new(code: '1212', start_date: 2.day.ago, end_date: 1.day.ago,
-                                client: client, car_category: car_category, user: user)
+        car = Car.create!(license_plate: 'ABC1234', color: 'Branco',
+                          car_model: car_model,
+                          mileage: 10_000)
+        rental = Rental.new(code: '1212', start_date: 2.days.ago,
+                            end_date: 1.day.ago,
+                            client: client, car_category: car_category,
+                            user: user)
         rental.save(validate: false)
-        CarRental.create(car: car, rental: rental, daily_rate: 30, start_mileage: 20,
-                         car_insurance: 30, third_party_insurance: 300)
-        newer_rental = Rental.new(code: '1212', start_date: Date.current, end_date: Date.current,
-          client: client, car_category: car_category, user: user)
-        CarRental.create(car: car, rental: newer_rental, daily_rate: 30, start_mileage: 20,
-            car_insurance: 30, third_party_insurance: 300)
+        CarRental.create(car: car, rental: rental, daily_rate: 30,
+                         start_mileage: 20,
+                         car_insurance: 30,
+                         third_party_insurance: 300)
+        newer_rental = Rental.new(code: '1212', start_date: Date.current,
+                                  end_date: Date.current,
+                                  client: client, car_category: car_category,
+                                  user: user)
+        CarRental.create(car: car, rental: newer_rental, daily_rate: 30,
+                         start_mileage: 20,
+                         car_insurance: 30,
+                         third_party_insurance: 300)
 
         result = newer_rental.car_statuses
 
@@ -121,24 +154,31 @@ describe Rental do
         client = Client.create!(name: 'Fulano da Silva', cpf: '127.587.748-60',
                                 email: 'fulanodasilva@teste.com')
         manufacturer = Manufacturer.create!(name: 'Renault')
-        car_category = CarCategory.create!(name: 'AM', daily_rate: 46.54, car_insurance: 28,
+        car_category = CarCategory.create!(name: 'AM', daily_rate: 46.54,
+                                           car_insurance: 28,
                                            third_party_insurance: 10)
-        car_model = CarModel.create!(name: 'Kwid', year: '2020', manufacturer: manufacturer,
-                                     motorization: '1.0', car_category: car_category,
+        car_model = CarModel.create!(name: 'Kwid', year: '2020',
+                                     manufacturer: manufacturer,
+                                     motorization: '1.0',
+                                     car_category: car_category,
                                      fuel_type: 'Flex')
-        car = Car.create!(license_plate: 'ABC1234', color: 'Branco', car_model: car_model,
-                          mileage: 10000)
-        rental = Rental.new(code: '1212', start_date: 1.day.from_now, end_date: 2.days.from_now,
-                                client: client, car_category: car_category, user: user)
+        car = Car.create!(license_plate: 'ABC1234', color: 'Branco',
+                          car_model: car_model,
+                          mileage: 10_000)
+        rental = Rental.new(code: '1212', start_date: 1.day.from_now,
+                            end_date: 2.days.from_now,
+                            client: client, car_category: car_category,
+                            user: user)
         rental.save(validate: false)
-        CarRental.create(car: car, rental: rental, daily_rate: 30, start_mileage: 20,
-                         car_insurance: 30, third_party_insurance: 300)
+        CarRental.create(car: car, rental: rental, daily_rate: 30,
+                         start_mileage: 20,
+                         car_insurance: 30,
+                         third_party_insurance: 300)
 
         result = rental.car_statuses
 
         expect(result).to eq 'avaliable'
       end
-
     end
   end
 end
